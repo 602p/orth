6dec5ae577636cebe932f725390624319166e5c7
Louis Goessling <louis@louis-laptop-debian2>
2016-11-14 16:49:47 -0600

    Tokenizer!

--- compiler/parse.py
+++ compiler/parse.py
@@ -1,3 +1,4 @@
+from tokenize import toktys, tokenize
 
 class ASTNode:
 	def __init__(self, **kwargs):
@@ -8,8 +9,23 @@ class ASTNode:
 		r = type(self).__name__
 		if self.attrs:
 			r+="("
-			r+=", ".join(k+"="+str(self.__dict__[v]) for k in self.attrs)
+			r+=", ".join(k+"="+str(self.__dict__[k]) for k in self.attrs)
 			r+=")"
 		return r
 
 	def __repr__(self): return str(self)
+
+def parse(tokens):
+	pass
+
+tokens=tokenize(r"""function (int a, int b) foo ->  bool does
+	int b=2
+	a+=1
+	if !(a>=b) do
+		return 1
+	done
+return 0
+""")
+
+print(tokens)
+print(parse(tokens))
--- compiler/tokenize.py
+++ compiler/tokenize.py
@@ -1,5 +1,15 @@
 import re, collections
 
+class DotAccessibleTokenDict(collections.OrderedDict):
+	def __getattr__(self, a):
+		if a.startswith("T_"):
+			return self[a]
+		return object.__getattribute__(self, a)
+	def __setattr__(self, a, v):
+		if a.startswith("T_"):
+			self[a]=v
+		object.__setattr__(self, a, v)
+
 class TokenType:
 	def __init__(self, regex, preceeding_in=None, capture=False, emit=True):
 		self.regex=re.compile(regex)
@@ -27,43 +37,38 @@ class Token:
 
 R_IDENTIFIER="[a-zA-Z_]\w*"
 
-T_ENDOFSTATEMENT=TokenType(";")
+toktys=DotAccessibleTokenDict()
+toktys.T_FUNCTIONDECL=TokenType("function")
+toktys.T_ARGLIST_START = TokenType(r"\(", ["T_FUNCTIONDECL"])
+toktys.T_ARGLIST_SEPERATOR = TokenType(",", ["T_ARGLIST_ELEMENT"])
+toktys.T_ARGLIST_ELEMENT = TokenType(R_IDENTIFIER+" +"+R_IDENTIFIER, ["T_ARGLIST_START", "T_ARGLIST_SEPERATOR"], capture=True)
+toktys.T_ARGLIST_END = TokenType(r"\)", ["T_ARGLIST_START", "T_ARGLIST_ELEMENT"])
+toktys.T_FUNCTIONDECL_NAME = TokenType(R_IDENTIFIER, ["T_ARGLIST_END"], capture=True)
+toktys.T_FUNCTIONDECL_RETURNS = TokenType("->", ["T_FUNCTIONDECL_NAME"])
+toktys.T_FUNCTIONDECL_DOES = TokenType("does", ["T_FUNCTIONDECL_NAME", "T_FUNCTIONDECL_RETURN_TYPE"])
+toktys.T_FUNCTIONDECL_RETURN_TYPE = TokenType(R_IDENTIFIER, ["T_FUNCTIONDECL_RETURNS"], capture=True)
 
-tokentypes=collections.OrderedDict((
-	("T_FUNCTIONDECL",TokenType("function")),
-	("T_ARGLIST_START",TokenType(r"\(", ["T_FUNCTIONDECL"])),
-	("T_ARGLIST_SEPERATOR",TokenType(",", ["T_ARGLIST_ELEMENT"])),
-	("T_ARGLIST_ELEMENT",
-		TokenType(R_IDENTIFIER+" +"+R_IDENTIFIER, ["T_ARGLIST_START", "T_ARGLIST_SEPERATOR"], capture=True)),
-	("T_ARGLIST_END",TokenType(r"\)", ["T_ARGLIST_START", "T_ARGLIST_ELEMENT"])),
-	("T_FUNCTION_NAME",TokenType(R_IDENTIFIER, ["T_ARGLIST_END"], capture=True)),
-	("T_FUNCTION_RETURNS",TokenType("->", ["T_FUNCTION_NAME"])),
-	("T_FUNCTIONDECL_DOES",TokenType("does")),
-	("T_FUNCTIONDECL_TYPE",TokenType(R_IDENTIFIER, ["T_FUNCTION_RETURNS"], capture=True)),
-
-	("T_IF_START",TokenType("if +", ["T_ENDOFSTATEMENT"])),
-	("T_IF_DONE",TokenType("done", ["T_ENDOFSTATEMENT"])),
-	("T_IF_DO",TokenType("do")),
-	("T_IF_ELSE",TokenType("else", ["T_ENDOFSTATEMENT"])),
+toktys.T_IF_START = TokenType("if +", ["T_ENDOFSTATEMENT"])
+toktys.T_IF_DONE = TokenType("done", ["T_ENDOFSTATEMENT"])
+toktys.T_IF_DO = TokenType("do")
+toktys.T_IF_ELSE = TokenType("else", ["T_ENDOFSTATEMENT"])
 	
-	("T_RETURN",TokenType("return +", ["T_ENDOFSTATEMENT"])),
+toktys.T_RETURN = TokenType("return +", ["T_ENDOFSTATEMENT"])
 
-	("T_VAR_DECL",TokenType(R_IDENTIFIER+" ?"+R_IDENTIFIER, ["T_ENDOFSTATEMENT"], capture=True)),
-	("T_NAME",TokenType(R_IDENTIFIER, capture=True)),
+toktys.T_VAR_DECL = TokenType(R_IDENTIFIER+" ?"+R_IDENTIFIER, ["T_ENDOFSTATEMENT"], capture=True)
+toktys.T_NAME = TokenType(R_IDENTIFIER, capture=True)
 
-	("T_AUGASSIGN",TokenType(r"(\+=)|(-=)|(\*=)|(/=)", ["T_NAME", "T_VAR_DECL"], capture=True)),
-	("T_ASSIGNMENT",TokenType("=", ["T_NAME", "T_VAR_DECL"])),
-	("T_PAREN_OPEN",TokenType(r"\(")),
-	("T_PAREN_CLOSE",TokenType(r"\)")),
+toktys.T_AUGASSIGN = TokenType(r"(\+=)|(-=)|(\*=)|(/=)", ["T_NAME", "T_VAR_DECL"], capture=True)
+toktys.T_ASSIGNMENT = TokenType("=", ["T_NAME", "T_VAR_DECL"])
+toktys.T_PAREN_OPEN = TokenType(r"\(")
+toktys.T_PAREN_CLOSE = TokenType(r"\)")
 
-	("T_COMPOUND_OPERATOR",TokenType("(>=)|(<=)|(!=)", capture=True)),
-	("T_OPERATOR",TokenType(r"[/\*\-\+%&|\^><!]", capture=True)),
+toktys.T_OPERATOR = TokenType(r"(>=)|(<=)|(!=)|[/\*\-\+%&|\^><!]", capture=True)
 
-	("T_INTEGER_LITERAL",TokenType(r"[0-9]+", capture=True)),
-	("T_ENDOFSTATEMENT", T_ENDOFSTATEMENT)
-))
+toktys.T_INTEGER_LITERAL = TokenType(r"[0-9]+", capture=True)
+toktys.T_ENDOFSTATEMENT = TokenType(";")
 
-for k,v in tokentypes.items():v.name=k
+for k,v in toktys.items():v.name=k
 
 def tokenize(text):
 	position=0
@@ -76,7 +81,7 @@ def tokenize(text):
 
 		if text[position] in "\r\n":
 			print("(adding T_ENDOFSTATEMENT)")
-			tokens.append(T_ENDOFSTATEMENT.make_token(";"))
+			tokens.append(toktys.T_ENDOFSTATEMENT.make_token(";"))
 			position+=1
 			continue
 
@@ -84,7 +89,7 @@ def tokenize(text):
 			position+=1
 			continue
 
-		for token in tokentypes.values():
+		for token in toktys.values():
 			if token.matches(text[position:], tokens[-1] if tokens else None):
 				
 				tok=token.make_token(text[position:])
@@ -97,13 +102,3 @@ def tokenize(text):
 			raise SyntaxError("No match at %i: %s" % (position, text[position:]))
 
 	return tokens
-
-print(tokenize(r"""
-function (int a, int b) foo ->  bool does
-	int b=2
-	a+=1
-	if !(a>=b) do
-		return 1
-	done
-return 0
-"""))

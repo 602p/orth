f1b60b630f641e257bc4624ee32c31318a855e59
Louis Goessling [lld5hp] <louis@goessling.com>
2017-04-25 20:38:08 -0500

    Add some more formatting stuff

--- docs/make_highlighted.py
+++ docs/make_highlighted.py
@@ -19,11 +19,19 @@ with open(os.path.basename(sys.argv[1])+".html", 'w') as out:
 	with open(sys.argv[1], 'r') as fd:
 		tokens=tokenize.tokenize(fd.read(), sys.argv[1], emit_whitespace=True)
 		print("\n".join(str(t) for t in tokens))
+		prev=None
 		for token in tokens:
 			if token.type.name=="T_ENDOFSTATEMENT":
 				out.write("<br/>")
+			elif token.type.name=="T_NAME" and prev.type.name=="T_CAST":
+				out.write("<span class=\"orth-T_NAME-cast-type\">"+html.escape(token.value)+"</span>")
+			elif token.type.name=="T_NAME" and prev.type.name in ["T_COLON", "T_DOUBLECOLON", "T_DOT"]:
+				out.write("<span class=\"orth-T_NAME-accessor\">"+html.escape(token.value)+"</span>")
+			elif token.type.name in ["T_ARGLIST_ELEMENT", "T_VAR_DECL"]:
+				out.write("<span class=\"orth-"+token.type.name+"-type\">"+token.value.split(" ")[0]+"</span> <span class=\"orth-"+token.type.name+"-name\">"+token.value.split(" ")[-1]+"</span>")
 			else:
 				out.write("<span class=\"orth-"+token.type.name+"\">"+html.escape(token.value)+"</span>")
+			prev=token
 	out.write("""
 
 		</pre>

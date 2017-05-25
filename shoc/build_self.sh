07eedc8f6da729dee11bad71ec1d2b01e557166f
Louis Goessling <louis@goessling.com>
2017-05-24 22:17:07 -0500

    New build tools

---
+++ shoc/build_self.sh
@@ -0,0 +1,8 @@
+cd src
+../shoc main.ort -s../lib -s../../lib -fno_strict_dup -fdo_llvm_out $@
+llc-3.5 llvm_out.ir
+gcc llvm_out.ir.s
+mv a.out ../shoc
+cd ..
+echo $(expr 1 + $(cat build_count.txt)) > build_count.txt
+echo "SHOC Built. Self-builds: " $(cat build_count.txt)
--- shoc/test_self.sh
+++
@@ -1,3 +0,0 @@
-cd src
-../shoc main.ort -s../../lib $@
-cd ..

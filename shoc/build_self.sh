d3aa430b3f9c92e680352c0742b3f40b1e209693
Louis Goessling <louis@goessling.com>
2017-05-30 09:18:43 -0500

    Improve tooling, hashmap

--- shoc/build_self.sh
+++ shoc/build_self.sh
@@ -1,9 +1,7 @@
 set -e
 cd src
-../shoc main.ort -s../lib -s../../lib -fno_strict_dup -fdo_llvm_out $@
-llc-3.5 llvm_out.ir
-gcc llvm_out.ir.s
-mv a.out ../shoc
+../shoc main.ort -fno_strict_dup -oshoc $@
+mv shoc ..
 cd ..
 echo $(expr 1 + $(cat build_count.txt)) > build_count.txt
 echo "SHOC Built. Self-builds: " $(cat build_count.txt)

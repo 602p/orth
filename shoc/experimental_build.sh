0cbae60ed99733c17ae3a8813efe6e5af8febd84
Louis Goessling <louis@goessling.com>
2017-05-30 11:38:30 -0500

    Misc tooling and opt

--- shoc/experimental_build.sh
+++ shoc/experimental_build.sh
@@ -1,6 +1,6 @@
 set -e
 cd src
-../shoc_good main.ort -fno_strict_dup -oshoc $@
+../shoc main.ort -fno_strict_dup -oshoc $@
 mv shoc ../shoc
 cd ..
 echo $(expr 1 + $(cat build_count.txt)) > build_count.txt

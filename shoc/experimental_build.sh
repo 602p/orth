ebfe5ae1a03fdd84bef5fb494151d4422f44345d
Louis Goessling <louis@goessling.com>
2017-06-04 21:47:16 -0500

    Count self-builds in the executable. Don't trust trust

--- shoc/experimental_build.sh
+++ shoc/experimental_build.sh
@@ -3,6 +3,3 @@ cd src
 ../shoc main.ort -fno_strict_dup -oshoc $@
 mv shoc ../shoc
 cd ..
-echo $(expr 1 + $(cat build_count.txt)) > build_count.txt
-echo
-echo "SHOC Built. Self-builds: " $(cat build_count.txt)

0a4a500e3fd212fd8b8cf88a1755b99fdcaaeb6e
Louis Goessling <louis@goessling.com>
2017-07-15 13:51:42 -0500

    Actually functional simple assembly emission

--- shoc/devel.sh
+++ shoc/devel.sh
@@ -1,3 +1,4 @@
 alias cfm="mv shoc shoc_good"
-alias rbt="./build_self.sh && ./shoc test.ort -fno_fold -fdump_gir -fdo_asm_out"
-alias t="./shoc test.ort -fno_fold -fdump_gir -fdo_asm_out"
+alias rbt="./build_self.sh && t"
+alias t="./shoc test.ort -fno_fold -fdump_gir -fdo_asm_out && cat assembly_out.s"
+alias fab="as assembly_out.s -o obj.o && gcc obj.o && ./a.out"

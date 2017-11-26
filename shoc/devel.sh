beed872e50b0462b10b89239470841ff00b799a1
Louis Goessling <louis@goessling.com>
2017-11-26 00:53:18 -0600

    Add options for building without libunwind

--- shoc/devel.sh
+++
@@ -1,4 +0,0 @@
-alias cfm="mv shoc shoc_good"
-alias rbt="./build_self.sh && t"
-alias t="./shoc test.ort -fno_fold -fdump_gir -fdo_asm_out && cat assembly_out.s"
-alias fab="as --32 assembly_out.s -o obj.o && gcc -m32 obj.o && ./a.out"
--- shoc/experimental_build.sh
+++ shoc/experimental_build.sh
@@ -1,5 +1,12 @@
 set -e
 cd src
-../shoc main.ort -fno_strict_dup -oshoc "--gcc_x../../lib/unwind/unwind_shim.o -lunwind" $@
+
+if [[ "${@#--no-unwind}" = "$@" ]]
+then
+    ../shoc main.ort -fno_strict_dup -oshoc "--gcc_x../../lib/unwind/unwind_shim.o -lunwind" $@
+else
+    ../shoc main.ort -fno_strict_dup -oshoc -s../../lib/nounwind_shim $@
+fi
+
 mv shoc ../shoc
 cd ..

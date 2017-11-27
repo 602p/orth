4018aa5df00b0cdc1d6032c61cab20264abd7cdc
Louis Goessling <louis@goessling.com>
2017-11-27 16:13:14 -0600

    Remove unused -fno_strict_dup flag

--- shoc/build_self.sh
+++ shoc/build_self.sh
@@ -3,9 +3,9 @@ cd src
 
 if [[ "${@#--no-unwind}" = "$@" ]]
 then
-    ../shoc_good main.ort -fno_strict_dup -oshoc "--gcc_x../../lib/unwind/unwind_shim.o -lunwind" $@
+    ../shoc_good main.ort -oshoc "--gcc_x../../lib/unwind/unwind_shim.o -lunwind" $@
 else
-    ../shoc_good main.ort -fno_strict_dup -oshoc -s../../lib/nounwind_shim $@
+    ../shoc_good main.ort -oshoc -s../../lib/nounwind_shim $@
 fi
 
 mv shoc ../shoc
